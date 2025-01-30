509168ef10c40bf23bb74def2263d6602b251f1b
SrDev-Henrique <halbuquerque2850@gmail.com>
2025-01-30 15:03:11 -0300

    Add scroll interactivity to NavBar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,34 +1,64 @@
 "use client";
 
 import { TiArrowSortedDown, TiArrowSortedUp } from "react-icons/ti";
+import { useWindowScroll } from "react-use";
 import React, { memo, useEffect, useState } from "react";
 import Image from "next/image";
 
 import { useMenu } from "@/contexts/MenuContext";
 import Button from "./Button";
 import OverlayMenu from "./OverlayMenu";
+import gsap from "gsap";
 
 const Navbar = memo(() => {
   const [isAudioPlaying, setIsAudioPlaying] = useState(false);
   const [isIndicatorActive, setIsIndicatorActive] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [isNavVisible, setIsNavVisible] = useState(true);
 
   const navContainerRef = React.useRef<HTMLDivElement>(null);
   const audioElementRef = React.useRef<HTMLAudioElement>(null);
 
   const { isMenuOpen, setIsMenuOpen } = useMenu();
 
+  const { y: currentScrollY } = useWindowScroll();
+
+  useEffect(() => {
+    if (currentScrollY === 0) {
+      setIsNavVisible(true);
+      navContainerRef.current?.classList.remove('floating-nav');
+    } else if (currentScrollY > lastScrollY) {
+      setIsNavVisible(false);
+      navContainerRef.current?.classList.add('floating-nav');
+    } else if (currentScrollY < lastScrollY) {
+      setIsNavVisible(true);
+      navContainerRef.current?.classList.add('floating-nav');
+    }
+
+    setLastScrollY(currentScrollY);
+  }, [currentScrollY, lastScrollY]);
+
+  useEffect(() => {
+    gsap.to(navContainerRef.current, {
+      y: isNavVisible ? 0 : -100,
+      opacity: isNavVisible ? 1 : 0,
+      duration: 0.1,
+      ease: "power1.out",
+    })
+  }, [isNavVisible])
+
   const toggleAudioIndicator = () => {
     setIsAudioPlaying((prev) => !prev);
 
     setIsIndicatorActive((prev) => !prev);
-  }
+  };
 
   useEffect(() => {
     if (audioElementRef.current) {
       console.log("Duração real do áudio:", audioElementRef.current.duration);
       audioElementRef.current.volume = 0.16;
     }
-  })
+  });
 
   useEffect(() => {
     if (isAudioPlaying) {
@@ -40,16 +70,16 @@ const Navbar = memo(() => {
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
-  }
+  };
   return (
     <>
       <div
         ref={navContainerRef}
-        className="fixed inset-x-0 top-4 z-[100] h-16 border-none transition-all duration-700 sm:inset-x-6"
+        className="fixed inset-x-0 top-2 z-[100] h-16 border-none transition-all duration-700 sm:inset-x-6"
       >
         <header className="absolute top-1/2 w-full -translate-y-1/2 flex justify-center">
           <nav className="relative flex w-full h-full items-center justify-between padding-4">
-            <div className="flex ml-4 sm:ml-0">
+            <div className="flex ml-2 ">
               <button
                 className="flex items-center space-x-0.5"
                 onClick={toggleAudioIndicator}
@@ -59,7 +89,7 @@ const Navbar = memo(() => {
                   alt="logo"
                   width={40}
                   height={40}
-                  className="mr-4 sm:ml-0 rounded-full filter brightness-75"
+                  className="mr-4 rounded-full filter brightness-75"
                 />
                 <audio
                   className="hidden"
