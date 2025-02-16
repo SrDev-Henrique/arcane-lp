660ffe12a11dcd5efea3440fca375e4bb980a3cd
SrDev-Henrique <halbuquerque2850@gmail.com>
2025-02-16 02:09:38 -0300

    Add images for Personagens' subsection

--- app/(sections)/piltover/components/Personagens.tsx
+++ app/(sections)/piltover/components/Personagens.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 const Personagens = () => {
   return (
-    <div className="text-white h-[100dvh] filter brightness-75 bg-piltover-character">
-      PERSONAGENS
+    <div className="text-white h-screen flex gap-[1.7%] justify-center filter brightness-100 bg-piltover-character">
+      <div className="min-w-[18%] bg-purple-50">PERSONAGENS</div>
+      <div className="min-w-[18%] bg-purple-50">PERSONAGENS</div>
+      <div className="min-w-[18%] bg-purple-50">PERSONAGENS</div>
+      <div className="min-w-[18%] bg-purple-50">PERSONAGENS</div>
+      <div className="min-w-[18%] bg-purple-50">PERSONAGENS</div>
     </div>
-  )
+  );
 }
 
 export default Personagens
