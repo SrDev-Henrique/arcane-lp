4848a3101d434191bfd683aed0cc39bcbfb9a2b1
SrDev-Henrique <halbuquerque2850@gmail.com>
2025-03-04 02:17:50 -0300

    Page Transitions - Add react-transition-group and TransitionContext to create TransitionComponent

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic";
+import Transition from "@/components/Transition";
 
 const Hero = dynamic(() => import("@/app/(sections)/hero/page"));
 const About = dynamic(() => import("@/app/(sections)/about/page"));
@@ -7,9 +8,11 @@ const Piltover = dynamic(() => import("@/app/(sections)/piltover/page"));
 export default function Home() {
   return (
     <main className="relative min-h-screen w-screen">
-      <Hero />
-      <About />
-      <Piltover />
+      <Transition>
+        <Hero />
+        <About />
+        <Piltover />
+      </Transition>
     </main>
   );
 }
