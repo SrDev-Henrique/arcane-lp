5b5585bf5458b30dbde5c836e1541717b88ff157
SrDev-Henrique <halbuquerque2850@gmail.com>
2025-03-25 02:48:15 -0300

    Lenis - Don't use lenis on touch devices

--- components/ConditionalNavBar.tsx
+++ components/ConditionalNavBar.tsx
@@ -12,27 +12,31 @@ export default function ConditionalNavbar() {
   const pathname = usePathname();
 
   useEffect(() => {
-    const lenis = new Lenis({
-      duration: 2.5,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothWheel: true,
-      orientation: "vertical",
-      gestureOrientation: "vertical",
-    });
-    lenisRef.current = lenis;
+    if (!("ontouchstart" in window)) {
+      const lenis = new Lenis({
+        duration: 2.5,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        smoothWheel: true,
+        orientation: "vertical",
+        gestureOrientation: "vertical",
+      });
 
-    function raf(time: number) {
-      lenis.raf(time);
+      lenisRef.current = lenis;
+
+      function raf(time: number) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+      }
+
       requestAnimationFrame(raf);
     }
-    requestAnimationFrame(raf);
   }, []);
 
   useEffect(() => {
     lenisRef.current?.stop();
     window.scrollTo({ top: 0, left: 0, behavior: "instant" });
     lenisRef.current?.start();
-  }, [pathname])
+  }, [pathname]);
 
   useEffect(() => {
     if (lenisRef.current) {
@@ -44,9 +48,5 @@ export default function ConditionalNavbar() {
     }
   }, [isMenuOpen]);
 
-  return (
-    <div>
-      {pathname === "/" && <Navbar />}
-    </div>
-  );
+  return <div>{pathname === "/" && <Navbar />}</div>;
 }
