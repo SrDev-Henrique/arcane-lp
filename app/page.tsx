9e1595c454ed95f4543fb2a9032fec3fddf8493e
SrDev-Henrique <halbuquerque2850@gmail.com>
2025-05-05 15:28:06 -0300

    Page - Uncomment all sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,19 @@
 import dynamic from "next/dynamic";
 
-// const Hero = dynamic(() => import("@/app/(sections)/hero/page"));
-// const About = dynamic(() => import("@/app/(sections)/about/page"));
-// const Piltover = dynamic(() => import("@/app/(sections)/piltover/page"));
-// const Zaun = dynamic(() => import("@/app/(sections)/zaun/page"));
+const Hero = dynamic(() => import("@/app/(sections)/hero/page"));
+const About = dynamic(() => import("@/app/(sections)/about/page"));
+const Piltover = dynamic(() => import("@/app/(sections)/piltover/page"));
+const Zaun = dynamic(() => import("@/app/(sections)/zaun/page"));
 const Historia = dynamic(() => import("@/app/(sections)/historia/page"));
 const End = dynamic(() => import("@/components/End"));
 
 export default function Home() {
   return (
     <main className="relative min-h-screen w-screen">
-      {/* <Hero /> */}
-      {/* <About /> */}
-      {/* <Piltover /> */}
-      {/* <Zaun /> */}
-      <div className="h-[100dvh] bg-black-light"></div>
+      <Hero />
+      <About />
+      <Piltover />
+      <Zaun />
       <Historia />
       <End />
     </main>
